52cb8a221e155c98dc99bc1de5680c7043d6f8a4
Drashti Patel <dr954742@dal.ca>
2024-07-21 15:46:19 -0300

    Added necessary files for the deployment on cloud run

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-
 const Dashboard = () => {
   return (
-    <div classname="flex justify-center items-center h-16">
+    <div className="flex flex-col items-center">
       <div className="text-center font-semibold text-4xl my-3">Dashboard</div>
-      <iframe
-        title="Looker Studio Report"
-        width="100%"
-        height="600"
-        src="https://lookerstudio.google.com/embed/reporting/86b613ee-217d-442e-a92a-2f09c61ac766/page/IHF6D"
-        allowFullScreen
-      ></iframe>
+      <div className="w-full">
+        <iframe
+          title="Looker Studio Report 1"
+          width="100%"
+          height="600"
+          src="https://lookerstudio.google.com/embed/reporting/86b613ee-217d-442e-a92a-2f09c61ac766/page/IHF6D"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="w-full mt-8">
+        <iframe
+          title="Looker Studio Report 2"
+          width="100%"
+          height="600"
+          src="https://lookerstudio.google.com/embed/reporting/86b613ee-217d-442e-a92a-2f09c61ac766/page/p_j3a0t9xbjd"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };
